cc467794771f521cd2374674b89df83e32baff74
larry-chunhui <chunhuiliu90@gmail.com>
2020-06-26 20:08:20 -0400

    finished lab 11

---
+++ lab11/dict/BinaryTreeNode.java
@@ -0,0 +1,81 @@
+/* BinaryTreeNode.java */
+
+package dict;
+
+/**
+ *  BinaryTreeNode represents a node in a binary tree.
+ *
+ *  DO NOT CHANGE THIS FILE.
+ **/
+class BinaryTreeNode {
+
+  /**
+   *  entry is a (key, value) pair stored in this node.
+   *  parent is the parent of this node.
+   *  leftChild and rightChild are the children of this node.
+   **/
+  Entry entry;
+  BinaryTreeNode parent;
+  BinaryTreeNode leftChild, rightChild;
+
+  /**
+   *  Construct a BinaryTreeNode with a specified entry; parent and children
+   *  are null.
+   **/
+  BinaryTreeNode(Entry entry) {
+    this(entry, null, null, null);
+  }
+
+  /**
+   *  Construct a BinaryTreeNode with a specified entry and parent; children
+   *  are null.
+   **/
+  BinaryTreeNode(Entry entry, BinaryTreeNode parent) {
+    this(entry, parent, null, null);
+  }
+
+  /**
+   *  Construct a BinaryTreeNode, specifying all four fields.
+   **/
+  BinaryTreeNode(Entry entry, BinaryTreeNode parent,
+                 BinaryTreeNode left, BinaryTreeNode right) {
+    this.entry = entry;
+    this.parent = parent;
+    leftChild = left;
+    rightChild = right;
+  }
+
+  public BinaryTreeNode min(){
+    BinaryTreeNode cur=this;
+    while(cur.leftChild!=null){
+      cur=cur.leftChild;
+    }
+    return cur;
+  }
+
+  public BinaryTreeNode max(){
+    BinaryTreeNode cur=this;
+    while(cur.rightChild!=null){
+      cur=cur.rightChild;
+    }
+    return cur;
+  }
+
+  /**
+   *  Express a BinaryTreeNode as a String.
+   *
+   *  @return a String representing the BinaryTreeNode.
+   **/
+  public String toString() {
+    String s = "";
+
+    if (leftChild != null) {
+      s = "(" + leftChild.toString() + ")";
+    }
+    s = s + entry.key().toString() + entry.value();
+    if (rightChild != null) {
+      s = s + "(" + rightChild.toString() + ")";
+    }
+    return s;
+  }
+}
